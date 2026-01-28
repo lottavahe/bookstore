83931347fba88651b0c8a517d458003d8ace0ac0
lottavahe <lotta.vahe@gmail.com>
2026-01-28 15:47:17 +0200

    initial commit

---
+++ src/test/java/projektiharjoitus/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package projektiharjoitus.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
