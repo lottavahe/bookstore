6096973add80773d29faec9faef40a03baae1a17
lottavahe <lotta.vahe@gmail.com>
2026-02-03 11:15:04 +0200

    tarkistin että päivitetty

--- src/main/java/projektiharjoitus/bookstore/web/BookController.java
+++ src/main/java/projektiharjoitus/bookstore/web/BookController.java
@@ -1,5 +1,16 @@
 package projektiharjoitus.bookstore.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
 public class BookController {
+    @GetMapping("/index")
+    public String showBookstore() {
+        return "index";
+    }
+    
 
 }
